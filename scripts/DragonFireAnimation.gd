eaa174cad7af432fc5a9e933d1f7a9d67913479f
Jono-Yinny-Attfield <johnathan.attfield@gmail.com>
2026-01-31 14:50:10 +0100

    ParticleSystemUpdates
    Added scripts for move ending in dragon attack

--- scripts/DragonFireAnimation.gd
+++ scripts/DragonFireAnimation.gd
@@ -1,9 +1,27 @@
 extends GPUParticles3D
 
+var move_start: Vector3
+@export var move_end: Vector3 = Vector3(endingX, endingY, endingZ)
+var endingX: float
+var endingY: float
+var endingZ: float
+#@export_range(0.000, 1000.000, 0.001) var EndingX: float
+#@export_range(0.000, 1000.000, 0.001) var EndingY: float
+#@export_range(0.000, 1000.000, 0.001) var EndingZ: float 
+
+# Debug for Dragon fire attack
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	move_start = global_position
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if Input.is_action_pressed("dragonfire"):
+		_attackmove()
+	pass
+
+func _attackmove() -> void:
 	pass
