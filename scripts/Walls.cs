384e82b042b85d8031268188fd02ece2a91afec8
Litt1eRebe1 <tilaniebresler@gmail.com>
2026-01-31 20:26:06 +0100

    real walls

--- scripts/Walls.cs
+++ scripts/Walls.cs
@@ -5,49 +5,66 @@ public partial class Walls : Node3D
 {
 	#region Nodes
 
-	private CsgBox3D _level1CollisionWalls2 => GetNode<CsgBox3D>("1m-BlockerWall2");
+	// First solid wall
+	private Node3D _solidWall1 => GetNode<Node3D>("CastleWall");
+	private CollisionShape3D _solidWall1Collision => GetNode<Node3D>("CastleWall").GetNode<MeshInstance3D>("Base").GetNode<StaticBody3D>("StaticBody3D").GetNode<CollisionShape3D>("CollisionShape3D");
 
-	private CsgBox3D _level1ObjectiveWalls2 => GetNode<CsgBox3D>("1m-ObjectiveWall2");
+	// First hidden wall
+	private Node3D _hiddenWall1 => GetNode<Node3D>("CastleWallBottomA");
+	private CollisionShape3D _hiddenWall1Collision => GetNode<Node3D>("CastleWallBottomA").GetNode<MeshInstance3D>("CastleWallSecret").GetNode<StaticBody3D>("StaticBody3D").GetNode<CollisionShape3D>("CollisionShape3D");
 
-	private CsgBox3D _endZoneWall => GetNode<CsgBox3D>("1m-EndZoneWall");
+	// End zone solid wall
+	private Node3D _endZoneWallNode => GetNode<Node3D>("CastleWallDoorClosed");
+	private CollisionShape3D _endZoneWallCollision => GetNode<Node3D>("CastleWallDoorClosed").GetNode<MeshInstance3D>("CastleWallDoor").GetNode<StaticBody3D>("StaticBody3D").GetNode<CollisionShape3D>("CollisionShape3D");
+	private CollisionShape3D _endZoneWallDoorCollision => GetNode<Node3D>("CastleWallDoorClosed").GetNode<MeshInstance3D>("CastleDoor").GetNode<StaticBody3D>("StaticBody3D").GetNode<CollisionShape3D>("CollisionShape3D");
 
-	private CsgBox3D _endZoneObjectiveWall => GetNode<CsgBox3D>("1m-EndZoneObjectiveWall");
+	// End zone hidden wall
+	private Node3D _endZoneHiddenWallNode => GetNode<Node3D>("CastleWallSecret");
+	private CollisionShape3D _endZoneHiddenWallCollision => GetNode<Node3D>("CastleWallSecret").GetNode<MeshInstance3D>("Base").GetNode<StaticBody3D>("StaticBody3D").GetNode<CollisionShape3D>("CollisionShape3D");
 
 	#endregion
 	public override void _Ready()
 	{
-		_level1ObjectiveWalls2.UseCollision = false;
-		_endZoneObjectiveWall.UseCollision = false;
+		_hiddenWall1Collision.SetDeferred("disabled", false);
+		_endZoneHiddenWallCollision.SetDeferred("disabled", false);
+
+		_hiddenWall1.Hide();
+		_endZoneHiddenWallNode.Hide();
+
+		_solidWall1.Show();
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
 
 	#region Level Wall Functions
 	public void EnableObjectiveWalls()
 	{
-		// transform the y of the collision wall to be under the map
-		_level1CollisionWalls2.UseCollision = false;
-		_level1CollisionWalls2.Hide();
-		_level1ObjectiveWalls2.Show();
+		// toggle first wall
+		_solidWall1Collision.SetDeferred("disabled", true);
+		_hiddenWall1Collision.SetDeferred("disabled", true);
+		_solidWall1.Hide();
+		_hiddenWall1.Show();
 
-		_endZoneWall.UseCollision = false;
-		_endZoneWall.Hide();
-		_endZoneObjectiveWall.Show();
-		
+		// toggle end zone wall
+		_endZoneWallCollision.SetDeferred("disabled", true);
+		_endZoneWallDoorCollision.SetDeferred("disabled", true);
+		_endZoneWallNode.Hide();
+		_endZoneHiddenWallNode.Show();
 	}
 
 	public void DisableObjectiveWalls()
 	{
-		_level1CollisionWalls2.UseCollision = true;
-		_level1CollisionWalls2.Show();
-		_level1ObjectiveWalls2.Hide();
+		// toggle first wall
+		_solidWall1Collision.SetDeferred("disabled", false);
+		_hiddenWall1Collision.SetDeferred("disabled", true);
+		_solidWall1.Show();
+		_hiddenWall1.Hide();
 
-		_endZoneWall.UseCollision = true;
-		_endZoneWall.Show();
-		_endZoneObjectiveWall.Hide();
+
+		// toggle end zone wall
+		_endZoneWallCollision.SetDeferred("disabled", false);
+		_endZoneWallDoorCollision.SetDeferred("disabled", false);
+		_endZoneHiddenWallNode.Hide();
+		_endZoneWallNode.Show();
 	}
 
 
